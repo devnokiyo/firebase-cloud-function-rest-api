a91eebbb303c67f0fc6882758e79a160f1530113
devnokiyo <devnokiyo@gmail.com>
2019-03-02 01:25:23 +0900

    Expressで書換え UserIDを指定してUsersを1件取得するGETメソッドを追加 Userを追加するPOSTメソッドを追加 Userを更新するPUTメソッドを追加 Userを削除するDELETEメソッドを追加

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,71 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
 
 admin.initializeApp();
-exports.users = functions.https.onRequest((req, res) => {
-    const ref = admin.database().ref('users');
-    ref.once("value", function(data) {
-        res.send(data);
+app.get('/:user_id', (req, res) => {
+  const ref = admin.database().ref('users');
+  ref.orderByChild('user_id').equalTo(req.params.user_id)
+    .once('value').then(data => {
+      return res.send(data);
+    })
+    .catch(error => {
+      res.status(404).send('No data available.');
     });
 });
+
+app.post('/', (req, res) => {
+  const ref = admin.database().ref('users');
+  ref.push({
+    user_id: req.body.user_id,
+    user_name: req.body.user_name,
+    age: Number(req.body.age)
+  }).then(data => {
+    return res.status(201).send(data);
+  })
+  .catch(error => {
+    res.status(400).send(error);      
+  });
+});
+
+app.put('/:id', (req, res) => {
+  const ref = admin.database().ref('users');
+  const updates = {};
+  updates[req.params.id] = {
+    user_id: req.body.user_id,
+    user_name: req.body.user_name,
+    age: Number(req.body.age)
+  };
+  ref.update(updates).then(data => {
+    return res.send(data);
+  })
+  .catch(error => {
+    res.status(400).send(error);      
+  });
+});
+
+app.delete('/:id', (req, res) => {
+  const ref = admin.database().ref(`users/${req.params.id}`);
+  ref.remove().then(data => {
+    return res.send(data);
+  })
+  .catch(error => {
+    res.status(400).send(error);      
+  });
+});
+
+app.get('/', (req, res) => {
+  const ref = admin.database().ref('users');
+  ref.once('value').then(data => {
+    return res.send(data);
+  })
+  .catch(error => {
+    res.status(404).send('No data available.');
+  });
+});
+
+exports.users = functions.https.onRequest(app);
